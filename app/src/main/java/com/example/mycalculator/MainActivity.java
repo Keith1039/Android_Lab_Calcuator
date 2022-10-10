a466722139a87302c01de50875013fe2b2373802
Keith Compere <keithcompere150@gmail.com>
2022-10-10 15:38:07 -0400

    working on it

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -15,7 +15,7 @@ import javax.script.ScriptException;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btn0,btn1, btn2, btn3, btn4, btn5,btn6, btn7,btn8,btn9, btn_clear, btn_plus, btn_equal;
+    Button btn0,btn1, btn2, btn3, btn4, btn5,btn6, btn7,btn8,btn9, btn_dot, btn_clear, btn_plus, btn_minus, btn_multi, btn_divide, btn_equal;
     TextView text_display;
 
     // This is to evaluate the math expression
@@ -31,24 +31,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn1 = (Button) findViewById(R.id.btn1);
         btn2 = (Button) findViewById(R.id.btn2);
         btn3 = (Button) findViewById(R.id.btn3);
-        btn4 = (Button) findViewById(R.id.button5);
-        btn5 = (Button) findViewById(R.id.button6);
-        btn6 = (Button) findViewById(R.id.button7);
-        btn7 = (Button) findViewById(R.id.button9);
-        btn8 = (Button) findViewById(R.id.button10);
-        btn9 = (Button) findViewById(R.id.button11);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
         btn_plus = (Button) findViewById(R.id.btn_plus);
+        btn_minus = (Button) findViewById(R.id.btn_minus);
+        btn_multi = (Button) findViewById(R.id.btn_multi);
+        btn_divide = (Button) findViewById(R.id.btn_divide);
         btn_equal = (Button) findViewById(R.id.btn_equal);
         btn_clear = (Button) findViewById(R.id.btn_clear);
-        text_display = (TextView) findViewById(R.id.textview_input_display);
+        btn_dot = (Button)findViewById(R.id.btn_dot);
+        text_display = (TextView) findViewById(R.id.text_display);
 
         setClickListeners();
     }
 
     private void setClickListeners() {
+        btn0.setOnClickListener(this);
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btn_dot.setOnClickListener(this);
         btn_plus.setOnClickListener(this);
+        btn_minus.setOnClickListener(this);
+        btn_multi.setOnClickListener(this);
+        btn_divide.setOnClickListener(this);
         btn_equal.setOnClickListener(this);
         btn_clear.setOnClickListener(this);
     }
@@ -62,9 +78,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn2:
                 addNumber("2");
                 break;
+            case R.id.btn3:
+                    addNumber("3");
+                    break;
+            case R.id.btn4:
+                addNumber("4");
+                break;
+            case R.id.btn5:
+                addNumber("5");
+                break;
+            case R.id.btn6:
+                addNumber("6");
+                break;
+            case R.id.btn7:
+                addNumber("7");
+                break;
+            case R.id.btn8:
+                addNumber("8");
+                break;
+            case R.id.btn9:
+                addNumber("9");
+                break;
+            case R.id.btn_dot:
+                addNumber(".");
+                break;
             case R.id.btn_plus:
                 addNumber("+");
                 break;
+            case R.id.btn_minus:
+                addNumber("-");
+                break;
+            case R.id.btn_multi:
+                addNumber("x");
+                break;
+            case R.id.btn_divide:
+                addNumber("/");
+                break;
+
             case R.id.btn_equal:
                 String result = null;
                 try {
